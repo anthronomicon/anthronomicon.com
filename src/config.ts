551a42d4d9229813c1d77af6b50f4766c945ac38
Tyler Reckart <tyler.reckart@gmail.com>
2023-08-23 14:53:13 -0400

    initial commit

---
+++ src/config.ts
@@ -0,0 +1,15 @@
+export default {
+  site: {
+    url: "https://anthronomicon.blog",
+    name: "Anthronomicon",
+  },
+  seo: {
+    title: "Anthronomicon",
+    author: "Tyler Reckart",
+    description: "",
+    sitename: "Anthronomicon",
+  },
+  integrations: {
+    gaTrackingId: "UA-70418516-2",
+  },
+};
